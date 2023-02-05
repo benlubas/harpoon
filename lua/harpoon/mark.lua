04ecfba45142935ae29e7de031fcc15e2137b377
benlubas <benmlubas@gmail.com>
2023-02-05 16:25:13 -0500

    well, now that's better

--- lua/harpoon/mark.lua
+++ lua/harpoon/mark.lua
@@ -245,7 +245,10 @@ function M.remove_empty_tail(_emit_on_changed)
     end
 end
 
+-- this runs on every buf leave, which makes me think that it's the problem
 function M.store_offset()
+    print('store offset')
+    local start = vim.loop.now()
     log.trace("store_offset()")
     local ok, res = pcall(function()
         local marks = harpoon.get_mark_config().marks
@@ -266,6 +269,8 @@ function M.store_offset()
         marks[idx].row = cursor_pos[1]
         marks[idx].col = cursor_pos[2]
     end)
+
+    print("store offset took: ", (vim.loop.now() - start) / 1000)
 
     if not ok then
         log.warn("store_offset(): Could not store offset:", res)
--- lua/harpoon/utils.lua
+++ lua/harpoon/utils.lua
@@ -7,58 +7,67 @@ local M = {}
 M.data_path = data_path
 
 function M.project_key()
-    return vim.loop.cwd()
+  return vim.loop.cwd()
 end
 
-function M.branch_key()
-    -- `git branch --show-current` requires Git v2.22.0+ so going with more
-    -- widely available command
-    local branch = M.get_os_command_output({
-        "git",
-        "rev-parse",
-        "--abbrev-ref",
-        "HEAD",
-    })[1]
+M.cached_branch_key = nil
 
-    if branch then
-        return vim.loop.cwd() .. "-" .. branch
-    else
-        return M.project_key()
-    end
+function M.branch_key()
+  if M.cached_branch_key then
+    return M.cached_branch_key
+  end
+  -- `git branch --show-current` requires Git v2.22.0+ so going with more
+  -- widely available command
+  local branch = M.get_os_command_output({
+    "git",
+    "branch",
+    "--show-current",
+  })[1]
+
+  if branch then
+    M.cached_branch_key = vim.loop.cwd() .. "-" .. branch
+  else
+    M.cached_branch_key = M.project_key()
+  end
+  return M.cached_branch_key
 end
 
 function M.normalize_path(item)
-    return Path:new(item):make_relative(M.project_key())
+  return Path:new(item):make_relative(M.project_key())
 end
 
 function M.get_os_command_output(cmd, cwd)
-    if type(cmd) ~= "table" then
-        print("Harpoon: [get_os_command_output]: cmd has to be a table")
-        return {}
-    end
-    local command = table.remove(cmd, 1)
-    local stderr = {}
-    local stdout, ret = Job:new({
-        command = command,
-        args = cmd,
-        cwd = cwd,
-        on_stderr = function(_, data)
-            table.insert(stderr, data)
-        end,
-    }):sync()
-    return stdout, ret, stderr
+  local start = vim.loop.now()
+  if type(cmd) ~= "table" then
+    print("Harpoon: [get_os_command_output]: cmd has to be a table")
+    return {}
+  end
+  local command = table.remove(cmd, 1)
+  local stderr = {}
+  local stdout, ret = Job:new({
+    command = command,
+    args = cmd,
+    cwd = cwd,
+    on_stderr = function(_, data)
+      table.insert(stderr, data)
+    end,
+  }):sync()
+
+  print("cmd time:", (vim.loop.now() - start) / 1000)
+
+  return stdout, ret, stderr
 end
 
 function M.split_string(str, delimiter)
-    local result = {}
-    for match in (str .. delimiter):gmatch("(.-)" .. delimiter) do
-        table.insert(result, match)
-    end
-    return result
+  local result = {}
+  for match in (str .. delimiter):gmatch("(.-)" .. delimiter) do
+    table.insert(result, match)
+  end
+  return result
 end
 
 function M.is_white_space(str)
-    return str:gsub("%s", "") == ""
+  return str:gsub("%s", "") == ""
 end
 
 return M
